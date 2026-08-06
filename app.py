8bf973340a8d5f3c7de1472011d1af2fa1a7b865
Pyxigue <koussadiaz@gmail.com>
2026-08-06 21:10:47 +0200

    Test

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def get_stream_url():
     
     cookie_file = get_cookie_path()
 
-    ydl_opts = { 
+    ydl_opts = {
         "format": "bestaudio/best",
         "quiet": True,
         "noplaylist": True,
@@ -110,8 +110,7 @@ def get_stream_url():
         "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
         "extractor_args": {
             "youtube": {
-                "player_client": ["web_embedded", "mweb"],
-                "player_skip": ["js", "configs"]
+                "player_client": ["music", "android", "mweb"]
             }
         }
     }
