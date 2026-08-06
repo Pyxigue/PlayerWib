b5f2d9fb17ef28f442b68cc32f1827c53b4b1a22
Pyxigue <koussadiaz@gmail.com>
2026-08-06 20:10:36 +0200

    Test4

--- app.py
+++ app.py
@@ -75,13 +75,15 @@ def get_stream_url():
         "cookiefile": "cookies.txt",
         "extractor_args": {
             "youtube": {
-                "player_client": ["web_creator", "ios", "mweb"]
+                "player_client": ["tv", "android"]
             }
         },
         "nocheckcertificate": True,
         "geo_bypass": True,
+        "retries": 10,
+        "fragment_retries": 10,
     }
-
+    
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(video_url, download=False)
